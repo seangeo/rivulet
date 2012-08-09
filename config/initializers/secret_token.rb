729e7fadb17be5fec13c829dac175d75716ebda6
Sean Geoghegan <sean@seangeo.me>
2012-08-09 23:19:41 +0930

    Empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rivulet::Application.config.secret_token = '6541e2079c57977b33640ed341f3b6190c2a4bd07838291668bc400bc91d02cebef1fad5508ce9d4c414ae366d681565e7f04223970af06108812d508300ef75'
